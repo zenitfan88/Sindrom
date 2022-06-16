f3205388072d646243658412a6385d4fd6a988e3
zenitfan88 <nikanenkov@rambler.ru>
2022-06-16 21:58:00 +0400

    Init commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNew() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 15, 18, 4, 15, 13, 8, 6, 11, 11, 12};
+        long expected1 = 18;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected1, actual);
+    }
+}
+
